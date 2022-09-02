00f5d30a82ad8ad6bbf031727330328f832a6632
Hussein Samy <hussein.samy02@gmail.com>
2022-09-02 03:25:07 +0200

    Adds starter code for products router

---
+++ src/routes/api/products.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+
+const products = express.Router();
+
+products.get('/', () => {
+    
+})
+products.get('/:id', () => {
+
+})
+products.post('/', () => {
+
+})
+products.get('/top5', () => {
+
+})
+products.get('/product-by-category', () => {
+
+})
+
+export default products;
