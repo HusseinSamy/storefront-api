99ef238e4182eb027961ae2187704b2255f8faee
Hussein Samy <hussein.samy02@gmail.com>
2022-09-02 03:32:41 +0200

    Adds starter code for users model

---
+++ src/models/users.ts
@@ -0,0 +1,12 @@
+export interface IUsers {
+    id: number,
+    firstName: string,
+    lastName: string, 
+    password: string
+}
+
+export class UsersModel {
+    async index (){}
+    async show (){}
+    async create (){}
+}
