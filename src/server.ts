1300bc4325d9f71d457b1893fa9b7c4dc9feeeaf
Hussein Samy <hussein.samy02@gmail.com>
2022-09-02 04:37:27 +0200

    Adds main router to the app

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import router from './routes';
 
 const app: express.Application = express()
 const PORT = 3000;
 
 app.use(bodyParser.json())
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
-
+app.use('/', router)
 app.listen(PORT, function () {
     console.log(`starting app on: ${PORT    }`)
 })
