ed8f97ce6430389d7fbd8793746e65e041a12a11
Hussein Samy <hussein.samy02@gmail.com>
2022-09-02 03:24:45 +0200

    Adds starter code for products model

---
+++ src/models/products.ts
@@ -0,0 +1,12 @@
+export interface IProduct {
+    id: number, 
+    name: string,
+    price: number,
+    category: string
+}
+
+export class ProductModel{
+    async index () {}
+    async show () {}
+    async create () {}
+}
