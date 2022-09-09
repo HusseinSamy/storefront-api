d5fccf6f7e93994d74ca7c686fc54bd5c0b77546
Hussein Samy <hussein.samy02@gmail.com>
2022-09-09 02:29:55 +0200

    Adds orders model

--- src/models/orders.ts
+++ src/models/orders.ts
@@ -1,12 +1,21 @@
+import { database } from "../database"
+
 export interface IOrder{
-    id:number
+    id?:number
     product_id: number,
     product_quantity: number,
     user_id: number
     status: string
 }
 
-export class OrderModel{
-    async show(id: number) {}
+export class OrdersModel{
+    //Token required
+    async show(id: number): Promise<IOrder[]> {
+        const connection = await database.connect();
+        const sql = `SELECT * FROM orders WHERE id = $1`;
+        const result = await connection.query(sql,[id]);
+        connection.release();
+        return result.rows;
+    }
 
 }
