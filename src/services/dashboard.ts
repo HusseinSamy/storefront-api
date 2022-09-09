042f3f30f1fcbf691e5f7dd858fbcb3eb481bf6a
Hussein Samy <hussein.samy02@gmail.com>
2022-09-09 04:19:03 +0200

    Adds code for getTop5PopularProducts function

--- src/services/dashboard.ts
+++ src/services/dashboard.ts
@@ -1,4 +1,18 @@
-export const getTop5PopularProducts = async()=> {}
+import { database } from "../database";
+
+export const getTop5PopularProducts = async()=> {
+
+    try{
+    const connection = await database.connect();
+    const sql = `SELECT * FROM products LIMIT 5`;
+    const result = await connection.query(sql);
+    connection.release();
+    return result.rows;
+    }
+    catch(err) {
+        throw new Error(`Cannot retrive data from the database: ${err}`);
+    }
+}
 export const  getProductByCategory = async(category: string)=> {}
 //Token required
 export const getCompletedOrdersByUser = (user_id: number)=> {}
