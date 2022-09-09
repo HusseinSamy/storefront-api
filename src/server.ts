c47ee1248d088cd675201d2ab9a9ad8c80d3ad8b
Hussein Samy <hussein.samy02@gmail.com>
2022-09-09 22:59:54 +0200

    Adds and use cookie-parser

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 import router from './routes';
 
 const app: express.Application = express()
 const PORT = 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use('/', router)
 app.listen(PORT, function () {
-    console.log(`starting app on: ${PORT    }`)
+    console.log(`starting app on: ${PORT}`)
 })
