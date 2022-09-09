16192dcbf5f242416a4b3d5708e9cb2df2af3e64
Hussein Samy <hussein.samy02@gmail.com>
2022-09-09 02:29:08 +0200

    Adds products model

--- src/models/products.ts
+++ src/models/products.ts
@@ -1,12 +1,51 @@
+import { database } from "../database";
+
 export interface IProduct {
-    id: number, 
+    id?: number, 
     name: string,
     price: number,
     category: string
 }
 
 export class ProductModel{
-    async index () {}
-    async show () {}
-    async create () {}
+    async index (): Promise<IProduct[]> {
+        try{
+            const connection = await database.connect();
+            const sql = `SELECT * FROM products`;
+            const result = await connection.query(sql);
+            connection.release();
+            return result.rows;
+        }
+        catch(err) {
+            throw new Error(`Cannot retrive data from the database: ${err}`);
+        }
+    }
+    async show (id: number): Promise<IProduct> {
+        try{
+            const connection = await database.connect();
+            const sql = `SELECT * FROM products WHERE id = $1`;
+            const result = await connection.query(sql, [id]);
+            connection.release();
+            return result.rows[0];
+        }
+        catch(err) {
+            throw new Error(`Cannot retrive data from the database: ${err}`);
+        }
+    }
+
+    //Requires token
+    async create (product: IProduct): Promise<IProduct> {
+        try{
+            const connection = await database.connect();
+            const sql = `INSERT INTO products (name, price, category) VALUES ($1,$2,$3) RETURNING *`;
+            const result = await database.query(sql, [product.name, product.price, product.category]);
+            connection.release();
+            return result.rows[0];
+        }
+        catch(err) {
+            throw new Error(`Cannot post data to the database: ${err}`);
+        }
+    } 
 }
+
+export default ProductModel;
