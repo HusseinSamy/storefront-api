d75c03e7966604451de61cfc071bd14650c6f514
Hussein Samy <hussein.samy02@gmail.com>
2022-09-09 22:58:21 +0200

    Adds authorization middleware

---
+++ src/middlewares/authorization.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const authorize = async(req:any, res:Response, next:NextFunction) => {
+    const token = req.cookies.token;
+    try{
+        const user = jwt.verify(token, process.env.TOKEN_SECRET!);
+        req.user = user;
+    }
+    catch(err){
+        res.status(401);
+        throw new Error(`Error happended while validating token: ${err}`);
+    }
+    next();
+}
+
+export default authorize;
