e75a5db66ddf909fd68dfd489a1cb32f2625ef3f
Hussein Samy <hussein.samy02@gmail.com>
2022-09-09 02:27:39 +0200

    Creates dashboard service

--- src/services/dashboard.ts
+++ src/services/dashboard.ts
@@ -1,5 +1,7 @@
 export class DashboardQueries{
     async getTop5PopularProducts(){}
     async getProductByCategory(category: string){}
+    //Token required
+    async getCompletedOrdersByUser(user_id: number) {}
 
 }
