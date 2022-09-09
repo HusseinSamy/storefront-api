671b6e91e725f1752106d47850edc12799a4127c
Hussein Samy <hussein.samy02@gmail.com>
2022-09-09 22:58:52 +0200

    Uses authorization middleware

--- src/handlers/orders.ts
+++ src/handlers/orders.ts
@@ -1,7 +1,7 @@
 import { Request, Response, Router } from "express";
 import dotenv from 'dotenv'
-import jwt from 'jsonwebtoken'
 import { OrdersModel } from "../models/orders";
+import authorize from "../middlewares/authorization";
 
 dotenv.config();
 
@@ -9,22 +9,15 @@ const Orders = new OrdersModel();
 
 export const show = async(req: Request, res: Response)=> {
     try{
-        jwt.verify(req.body.token, process.env.TOKEN_SECRET!);
-    }
-    catch(err){
-        res.status(401);
-        throw new Error(`Error happened while verifying the token: ${err}`)
-    }
-    try{
         const result = await Orders.show(+req.params.id);
         res.send(result);
     }
     catch(err){
         throw new Error(`Error happened while retrieving orders from database: ${err}`);
-    }
+}
 }
 const ordersRouter = (app: Router) => {
-    app.get('/:id', show);
+    app.get('/:id', authorize, show);
 }
 
 export default ordersRouter;
