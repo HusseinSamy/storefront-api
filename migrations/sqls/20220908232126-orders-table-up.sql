ca9deb7c6bd1e82d6e93662f8f0f30110289c553
Hussein Samy <hussein.samy02@gmail.com>
2022-09-09 22:55:11 +0200

    Edits orders table migration

--- migrations/sqls/20220908232126-orders-table-up.sql
+++ migrations/sqls/20220908232126-orders-table-up.sql
@@ -1,14 +1,8 @@
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    product_id integer NOT NULL,
     user_id integer NOT NULL,
-    product_quantity integer NOT NULL,
     status text,
-    CONSTRAINT product_id FOREIGN KEY (product_id)
-        REFERENCES public.products (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT user_id FOREIGN KEY (product_quantity)
+     CONSTRAINT user_id FOREIGN KEY (user_id)
         REFERENCES public.users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
