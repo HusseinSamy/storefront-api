feeae61390566d13fbeefc9fab725d6baa7e4159
Hussein Samy <hussein.samy02@gmail.com>
2022-09-10 09:26:41 +0200

    Removes /product-by-category endpoint

--- src/routes/api/products.ts
+++ src/routes/api/products.ts
@@ -4,8 +4,5 @@ import productsRouter from '../../handlers/products';
 const products = express.Router();
 
 productsRouter(products);
-products.get('/product-by-category', () => {
-
-})
 
 export default products;
