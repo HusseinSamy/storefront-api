29f795b8cfdd6ebe9ed4349c64bb6f624bb80b26
Hussein Samy <hussein.samy02@gmail.com>
2022-09-10 09:22:12 +0200

    Adds functionallity of the getProductByCategory & getCompletedOrdersByUser funcitons

--- src/services/dashboard.ts
+++ src/services/dashboard.ts
@@ -1,7 +1,8 @@
 import { database } from "../database";
+import { Order } from "../models/orders";
+import { Product } from "../models/products";
 
-export const getTop5PopularProducts = async()=> {
-
+export const getTop5PopularProducts = async(): Promise<Product[]> => {
     try{
     const connection = await database.connect();
     const sql = `SELECT * FROM products LIMIT 5`;
@@ -13,6 +14,28 @@ export const getTop5PopularProducts = async()=> {
         throw new Error(`Cannot retrive data from the database: ${err}`);
     }
 }
-export const  getProductByCategory = async(category: string)=> {}
+export const getProductByCategory = async(category: string): Promise<Product[]> => {
+     try{
+    const connection = await database.connect();
+    const sql = `SELECT * FROM products WHERE category = $1`;
+    const result = await connection.query(sql,[category]);
+    connection.release();
+    return result.rows;
+    }
+    catch(err) {
+        throw new Error(`Cannot retrive data from the database: ${err}`);
+    }
+}
 //Token required
-export const getCompletedOrdersByUser = (user_id: number)=> {}
+export const getCompletedOrdersByUser = async(user_id: number): Promise<Order[]> => {
+    try{
+        const connection = await database.connect();
+        const sql = `SELECT * FROM orders WHERE user_id = $1 AND status = 'completed'`;
+        const result = await connection.query(sql,[user_id]);
+        connection.release();
+        return result.rows;
+        }
+        catch(err) {
+            throw new Error(`Cannot retrive data from the database: ${err}`);
+        }
+}
