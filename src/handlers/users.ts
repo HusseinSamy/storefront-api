9591e23032efdeb164599bbdabf6d0c0936d3b1f
Hussein Samy <hussein.samy02@gmail.com>
2022-09-10 09:26:08 +0200

    Adds token to the cookie and adds return types to functions

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, Router } from 'express'
-import { IUser, UsersModel } from '../models/users';
+import { User, UsersModel } from '../models/users';
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 import authorize from '../middlewares/authorization';
@@ -7,42 +7,46 @@ import authorize from '../middlewares/authorization';
 dotenv.config();
 const Users = new UsersModel();
 
-const index = async (_req: Request, res: Response) =>{
+const index = async (_req: Request, res: Response): Promise<User[]>  =>{
     try{
         const result = await Users.index();
         res.send(result);
+        return result;
     }
     catch(err){
         throw new Error(`Error happened while returning all users: ${err}`);
     }
 }
 
-const show = async (req: Request, res: Response) =>{
+const show = async (req: Request, res: Response): Promise<User> =>{
     try{
         const result = await Users.show(+req.params.id);
         res.send(result);
+        return result;
     }
     catch(err){
         throw new Error(`Error happened while returning user with id ${req.params.id}: ${err}`);
     }
 }
 
-const create = async (req: Request, res: Response) => {
+const create = async (req: Request, res: Response): Promise<User> => {
     
-    const user: IUser = {
+    const user: User = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     password: req.body.password
 }
-    try{
-        const newUser = await Users.create(user);
-        const token = jwt.sign({user: newUser}, process.env.TOKEN_SECRET!);
-        res.cookie("token", token,{httpOnly: true});
-        res.send(token);
-    }
-    catch(err){
-        res.send(`Error happened while creating user: ${err}`);
-    }
+try{
+    const newUser = await Users.create(user);
+    const token = jwt.sign({user: newUser}, process.env.TOKEN_SECRET!);
+    res.cookie("token", token,{httpOnly: true});
+    res.send(token);
+    return newUser;
+}
+catch(err){
+    throw new Error(`Error happened while creating user: ${err}`);
+}
+
 }
 const usersRouter = (app: Router) => {
     app.get('/', authorize, index);
