9e4b98d615f3658b26c1cf87bfbe66b3f4293a71
Hussein Samy <hussein.samy02@gmail.com>
2022-09-17 21:15:20 +0200

    Allows CORS

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import router from './routes';
+import cors from 'cors';
 
 const app: express.Application = express()
 const PORT = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
 
