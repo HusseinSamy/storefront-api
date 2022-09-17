1c0c7f35c0b7867d527cb505310b10e2314336cc
Hussein Samy <hussein.samy02@gmail.com>
2022-09-17 21:15:06 +0200

    Adds testing database

--- src/database.ts
+++ src/database.ts
@@ -3,16 +3,33 @@ import { Pool } from 'pg'
 
 dotenv.config();
 
+const ENV = process.env.ENV;
+
+export let database: Pool;
+
 export const {
     POSTGRES_DB,
+    POSTGRES_TEST_DB,
     POSTGRES_HOST,
     POSTGRES_USER,
     POSTGRES_PASSWORD,
 } = process.env;
 
-export const database = new Pool({
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    host: POSTGRES_HOST,
-    password: POSTGRES_PASSWORD,
-})
+if(ENV === 'dev') {
+
+    database = new Pool({
+        database: POSTGRES_DB,
+        user: POSTGRES_USER,
+        host: POSTGRES_HOST,
+        password: POSTGRES_PASSWORD,
+    })
+}
+if(ENV === 'test') {
+
+    database = new Pool({
+        database: POSTGRES_TEST_DB,
+        user: POSTGRES_USER,
+        host: POSTGRES_HOST,
+        password: POSTGRES_PASSWORD,
+    })
+}
