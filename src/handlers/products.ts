68e003218c8702997305b0d01bbe0c97a8dcdff1
Hussein Samy <hussein.samy02@gmail.com>
2022-09-24 15:20:21 +0200

    Changes the return type of the products endpoints

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -11,35 +11,40 @@ dotenv.config();
 
 const Products = new ProductModel();
 
-const index = async (_req: Request, res: Response): Promise<Product[]> => {
+const index = async (
+  _req: Request,
+  res: Response<Product[] | string>
+): Promise<Response<Product[] | string>> => {
   try {
     const result = await Products.index();
-    res.send(result);
-    return result;
+    return res.send(result);
   } catch (err) {
-    throw new Error(`error happened while retrieving all product: ${err}`);
+    return res.send(`error happened while retrieving all product`);
   }
 };
-const show = async (req: Request, res: Response): Promise<Product | null> => {
+const show = async (
+  req: Request,
+  res: Response<Product | string>
+): Promise<Response<Product | string>> => {
   try {
     const result = await Products.show(+req.params.id);
-    res.send(result);
     if (result) {
-      return result;
+      return res.send(result);
     } else {
       throw new Error(
         `error happened while retrieving product with id ${req.params.id}`
       );
     }
   } catch (err) {
-    res.status(400);
-    res.send(err);
-    return null;
+    return res.status(400).send((err as Error).message);
   }
 };
 
 //Requires token
-const create = async (req: Request, res: Response): Promise<Product> => {
+const create = async (
+  req: Request,
+  res: Response<Product | string>
+): Promise<Response<Product | string>> => {
   const product: Product = {
     name: req.body.name,
     price: req.body.price,
@@ -47,31 +52,31 @@ const create = async (req: Request, res: Response): Promise<Product> => {
   };
   try {
     const result = await Products.create(product);
-    res.send(result);
-    return result;
+    return res.send(result);
   } catch (err) {
-    res.send(err);
-    throw new Error(`error happened while creating product: ${err}`);
+    return res.send(`error happened while creating product`);
   }
 };
-const top5 = async (_req: Request, res: Response): Promise<Product[]> => {
+const top5 = async (
+  _req: Request,
+  res: Response<Product[] | string>
+): Promise<Response<Product[] | string>> => {
   try {
     const result = await getTop5PopularProducts();
-    res.send(result);
-    return result;
+    return res.send(result);
   } catch (err) {
-    throw new Error(`error happened while retrieving last 5 products: ${err}`);
+    return res.send(`error happened while retrieving last 5 products`);
   }
 };
-const byCategory = async (req: Request, res: Response): Promise<Product[]> => {
+const byCategory = async (
+  req: Request,
+  res: Response<Product[] | string>
+): Promise<Response<Product[] | string>> => {
   try {
     const result = await getProductByCategory(req.body.category);
-    res.send(result);
-    return result;
+    return res.send(result);
   } catch (err) {
-    throw new Error(
-      `error happened while filtering products by category: ${err}`
-    );
+    return res.send(`error happened while filtering products by category`);
   }
 };
 
